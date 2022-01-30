1bf252509f3554447e04e51b7ba7ed1f2574adbc
sdnc <dza.sara@gmail.com>
2022-01-31 10:48:55 +1100

    Add balloon game

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private Vector3 spawnPos = new Vector3(35, 0, 0);
     private float startDelay = 2.0f;
     private float repeatRate = 2.0f;
     public PlayerController playerControllerScript;
